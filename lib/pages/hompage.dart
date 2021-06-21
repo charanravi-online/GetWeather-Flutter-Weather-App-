6601cdae2ec36edd26c6556b1cc3d9f646f426cb
Charan R <charanravi.online@gmail.com>
2021-06-21 16:30:51 +0530

    added api

--- lib/pages/hompage.dart
+++ lib/pages/hompage.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +10,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController searchController = new TextEditingController();
+  String url =
+      "https://api.edamam.com/search?q=chicken&app_id=b5bda55b&app_key=b05d2191964b86ed8eeaca98180eb719";
+
+  getRecipe(String query) async {
+    String url =
+        "https://api.edamam.com/search?q=$query&app_id=b5bda55b&app_key=b05d2191964b86ed8eeaca98180eb719";
+    Response response = await get(Uri.parse(url));
+    Map data = jsonDecode(response.body);
+    log(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("I AM HOME"),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Colors.deepOrangeAccent,
+              // gradient: LinearGradient(
+              //     colors: [Color(0xff213A50), Color(0xff071938)]),
+            ),
+          ),
+          Column(
+            children: [
+              //Search Bar
+              SafeArea(
+                child: Container(
+                  //Search Wala Container
+
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") ==
+                              "") {
+                            print("Blank search");
+                          } else {
+                            getRecipe(searchController.text);
+                          }
+                        },
+                        child: Container(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: "Search"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Let's get cooking!",
+                      style: TextStyle(fontSize: 33, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    // Text(
+                    //   "",
+                    //   style: TextStyle(fontSize: 20, color: Colors.white),
+                    // )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
