bf920a431eb7533c4b14f580878c8aca610d24d4
Charan R <charanravi.online@gmail.com>
2021-06-26 14:53:35 +0530

    added scrollable widgets success

--- lib/pages/hompage.dart
+++ lib/pages/hompage.dart
@@ -51,71 +51,86 @@ class _HomePageState extends State<HomePage> {
               //     colors: [Color(0xff213A50), Color(0xff071938)]),
             ),
           ),
-          Column(
-            children: [
-              //Search Bar
-              SafeArea(
-                child: Container(
-                  //Search Wala Container
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                //Search Bar
+                SafeArea(
+                  child: Container(
+                    //Search Wala Container
 
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24)),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          if ((searchController.text).replaceAll(" ", "") ==
-                              "") {
-                            print("Blank search");
-                          } else {
-                            getRecipe(searchController.text);
-                          }
-                        },
-                        child: Container(
-                          child: Icon(
-                            Icons.search,
-                            color: Colors.blueAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24)),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if ((searchController.text).replaceAll(" ", "") ==
+                                "") {
+                              print("Blank search");
+                            } else {
+                              getRecipe(searchController.text);
+                            }
+                          },
+                          child: Container(
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                           ),
-                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                         ),
+                        Expanded(
+                          child: TextField(
+                            controller: searchController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, hintText: "Search"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Let's get cooking!",
+                        style: TextStyle(fontSize: 33, color: Colors.white),
                       ),
-                      Expanded(
-                        child: TextField(
-                          controller: searchController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: "Search"),
-                        ),
-                      )
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // Text(
+                      //   "",
+                      //   style: TextStyle(fontSize: 20, color: Colors.white),
+                      // )
                     ],
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Let's get cooking!",
-                      style: TextStyle(fontSize: 33, color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    // Text(
-                    //   "",
-                    //   style: TextStyle(fontSize: 20, color: Colors.white),
-                    // )
-                  ],
+                Container(
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 120,
+                      itemBuilder: (context, index) {
+                        return MyText();
+                      }),
                 ),
-              )
-            ],
+              ],
+            ),
           )
         ],
       ),
     );
   }
+}
+
+Widget MyText() {
+  return Text("MY TEXT WIDGET IS HERE");
 }
